ba2a325a64e5c41de213451fc69420a0a1ad4ae3
gunjansharma9 <gunjansharmahapur@gmail.com>
2025-03-04 03:00:02 +0530

    admin panel done,working on doctor panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,24 +10,34 @@ import AddDoctor from './pages/Admin/AddDoctor';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Admin/Dashboard';
 import AllApointments from './pages/Admin/AllApointments';
+import { DoctorContext } from './context/DoctorContext';
+import DoctorDashboard from './pages/Doctor/DoctorDashboard';
+import DoctorAppointment from './pages/Doctor/DoctorAppointment';
+import DoctorProfile from './pages/Doctor/DoctorProfile';
 
 const App = () => {
   const {aToken} = useContext(AdminContext)
+  const {dToken} = useContext(DoctorContext)
 
-  return aToken ? (
+  return aToken || dToken? (
     <div className='bg-[#F8F9Fd]'>
       <ToastContainer/>
       <Navbar/>
       <div className='flex items-start'>
         <Sidebar/>
         <Routes>
+        {/* Admin Route */}
           <Route path='/' element={<></>} />
           <Route path='/admin-dashboard' element = {<Dashboard/>} />
           <Route path='/all-appointments' element={<AllApointments/>} />
           <Route path='/add-doctor' element={<AddDoctor/>} />
           <Route path='/doctor-list' element={<DoctorsList/>} />
-        </Routes>
 
+        {/* Doctor Route */}
+          <Route path='/doctor-dashboard' element={<DoctorDashboard/>} />
+          <Route path='/doctor-appointments' element={<DoctorAppointment/>} />
+          <Route path='/doctor-profile' element={<DoctorProfile/>} />
+        </Routes>
       </div>
     </div>
   ) : (
