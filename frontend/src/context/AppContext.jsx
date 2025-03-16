c37b28b66298db7f25b23ef0db0aeca6156163e4
GUNJAN SHARMA <GUNJANSHARMAHAPUR@GMAIL.COM>
2025-03-16 23:41:25 +0530

    Updated AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
     const currencySymbol = '$'
-    const backendUrl = "http://localhost:4000"
+    const backendUrl = "https://med-well-backend.onrender.com"
     const [doctors,setDoctors] = useState([])
     const [token,setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
     const [userData,setUserData] = useState(false)
