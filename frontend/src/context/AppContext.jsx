8aa55d3aee2612aefd1c08557345000c482b15c7
gunjansharma9 <gunjansharmahapur@gmail.com>
2025-03-16 23:40:25 +0530

    debugging

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useEffect, useState } from "react";
-// import { doctors } from "../assets/assets";
 import axios from 'axios'
 import { toast } from "react-toastify";
 
@@ -7,7 +6,7 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
     const currencySymbol = '$'
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "http://localhost:4000"
     const [doctors,setDoctors] = useState([])
     const [token,setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
     const [userData,setUserData] = useState(false)
@@ -51,7 +50,8 @@ const AppContextProvider = (props) => {
         setToken,
         backendUrl,
         userData,setUserData,
-        loadUserProfileData
+        loadUserProfileData,
+        backendUrl
     }
 
     useEffect(() => {
